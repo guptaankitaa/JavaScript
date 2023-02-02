559e5056a9bfd617468c48bff273f5affaa7dcdf
Ankita Gupta <112807231+Ankita-k-codes@users.noreply.github.com>
2023-02-02 22:18:07 +0530

    Create file3.js

---
+++ file3.js
@@ -0,0 +1,148 @@
+//-----------------Expression and Operator--------------
+undefined
+
+//combination of operands and operator is known as expression
+undefined
+
+//---------assignment operator---------
+
+undefined
+var x = 4;
+undefined
+
+console.log(x);
+VM1391:1 4
+undefined
+
+console.log("value" + x);
+VM1455:1 value4
+undefined
+
+//-------- Arithmetic operator --------
+
+undefined
+3 + 3
+6
+10 - 5
+5
+20/2
+10
+5*2
+10
+3%2
+1
+
+//-------- Increment or decrement operator -------
+
+undefined
+var num = 15;
+undefined
+
+var newNum = num++;
+undefined
+
+console.log(num);
+VM1873:1 16
+undefined
+
+console.log(newNum);
+VM1935:1 15
+
+
+//---------- postfix x++ ----------
+
+undefined
+var num = 15;
+undefined
+var newnum = num++;
+undefined
+console.log(num);
+VM279:1 16
+undefined
+console.log(newnum);
+VM350:1 15
+undefined
+
+//--------prefix ++x ---------
+
+undefined
+var num = 15;
+undefined
+var newnum = ++num;
+undefined
+console.log(num);
+VM414:1 16
+undefined
+console.log(newnum);
+VM419:1 16
+undefined
+
+//-------comparison operator---------
+
+undefined
+var a = 30;
+undefined
+var b = 20;
+undefined
+console.log(a == b);
+VM616:1 false
+undefined
+console.log(a != b);
+VM638:1 true
+undefined
+console.log(a > b);
+VM652:1 true
+undefined
+console.log(a >= b);
+VM664:1 true
+undefined
+console.log(a <= b);
+VM674:1 false
+undefined
+
+//-------- logical operator -----------
+
+undefined
+var a = 30;
+undefined
+var b = -20;
+undefined
+
+//--------- AND OPERATOR (&&)
+
+undefined
+console.log(a > b && b > 0);
+VM1021:1 false
+undefined
+
+//if all the expression are true then only it will give true
+undefined
+
+//-------- OR OPERATOR ( || )
+
+undefined
+console.log(a > b || b > 0);
+VM1253:1 true
+undefined
+
+//if any one expression is true then it will give true
+undefined
+
+//-------- NOT OPERATOR ( ! )
+undefined
+
+console.log(!(a > b || b > 0));
+VM1465:1 false
+undefined
+
+//takes truth to falsity and vice versa
+undefined
+
+//-------- string operator("")
+undefined
+
+"hello " + " Ankita";
+'hello  Ankita' 
+
+// (+) this is known as concatination
+undefined
