f894ea3a62bb5506ff378b0ccad415589aabbef4
Ankita Gupta <112807231+Ankita-k-codes@users.noreply.github.com>
2023-02-03 22:31:00 +0530

    Create file4.js

---
+++ file4.js
@@ -0,0 +1,99 @@
+//------- challeng------
+undefined
+
+3**3
+27
+3*2
+6
+
+//is known as exponentiation operator
+undefined
+
+//-----------Swap two number-----
+undefined
+
+a = 4;
+4
+b = 5;
+5
+c = b;
+5
+b = a;
+4
+a = c;
+5
+
+//-------Swap using third variable-------
+undefined
+
+a = 2
+2
+b = 4;
+4
+a = a + b;
+6
+b = a - b;
+2
+a = a - b;
+4
+
+
+//------ Diff btw == and === -----
+undefined
+
+n = 3;
+3
+s = "3";
+'3'
+n == s;
+true
+n === s;
+false
+
+//bcoz == check only the value and === check value and data type 
+undefined
+
+//---------control statement and loop------
+undefined
+
+num = 5;
+5
+if(num = 5){
+}
+undefined
+if(num = 5){
+}
+undefined
+if( num == 5){
+ " num is 5"
+}
+else{ 
+" num is not 5"
+}
+' num is 5'
+
+//write a program to check whether a year is leap year or not
+undefined
+
+year = 2020;
+2020
+var year = 2020;
+undefined
+debugger;
+undefined
+if(year % 4 === 0){                                                                                                                                                                   
+if(year % 100 === 0){                                                                                                                                                                 
+if(year % 400 === 0){                                                                                                                           
+  console.log("year " + year + " is a leap year");                                                                                                                                     
+} else{                                                                                                                                                                         
+  console.log( "year " + year + " is not a leap year");                                                                                                                                   
+}}                                                                                                                                                                                      
+  else{                                                                                                                                                                                       
+    console.log("year " + year + " is a leap year");}}                                                                                                                                 
+ else{ console.log( "year " + year +" is not a leap year");}
+VM2298:4 year 2020 is a leap year
+undefined
+if(score = 0){ (" we WON ");} else{ (" we lose ")};
+' we lose '
+//0, " ", undefine , null, nan, false every tym else condition will work 
+undefined
